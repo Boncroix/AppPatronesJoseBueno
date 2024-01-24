cbecbda6bf516398dd18b13568ddca13af6ca983
Boncroix <boncroix@gmail.com>
2024-01-24 14:26:00 +0100

    Crear proyecto estructura de carpetas y crear la vista de login

---
+++ DragonBallMVVM/DragonBallMVVM/Presentation/Views/Login/LoginViewController.swift
@@ -0,0 +1,16 @@
+//
+//  LoginViewController.swift
+//  DragonBallMVVM
+//
+//  Created by Jose Bueno Cruz on 24/1/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
